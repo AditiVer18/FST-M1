c38bd3b2a7ec7c7c6381220b1bac75200da4b46e
AditiVer18 <Aditi.Verma3@ibm.com>
2024-06-20 00:32:50 +0530

    Add files via upload

---
+++ SQL/Activity2.sql
@@ -0,0 +1,7 @@
+INSERT INTO Salesman(salesman_id, salesman_name, salesman_city, commission)
+VALUES ('5001', 'James Hoog', 'New York', '15')
+VALUES ('5002', 'Nail Knite', 'Paris', '13')
+VALUES ('5003', 'Pit Alex', 'London', '11')
+VALUES ('5006', 'McLyon', 'Paris', '14')
+VALUES (5007, 'Paul Adam', 'Rome', 13)
+VALUES (5004, 'Lauson Hen', 'San Jose', 12);
---
+++ SQL/activity3.sql
@@ -0,0 +1,11 @@
+--Show data from the salesman_id and salesman_city columns
+	
+	Select salesman_id , salesman_city  from Salesman;
+
+--Show data of salesman from Paris
+
+	Select salesman_id , salesman_city  from Salesman;
+
+--Show salesman_id and commission of Paul Adam
+
+	Select salesman_id,commission from Salesman where salesman_name ='Paul Adam';
